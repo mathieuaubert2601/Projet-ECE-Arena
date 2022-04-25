4dddb326d5a4fea041b648c69f373a402f0422cd
mathieuaubert2601 <mathieu.aubert@edu.ece.fr>
2022-04-25 11:02:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Projet-ECE-Arena
+ 
---
+++ header.h
@@ -0,0 +1,13 @@
+#ifndef HEADER_H_INCLUDED
+#define HEADER_H_INCLUDED
+
+///Déclaration des bibliothèques
+#include <stdio.h>
+#include <stdlib.h>
+
+///Déclaration des structures
+
+///Déclaration des prototypes des sous programmes
+
+
+#endif // HEADER_H_INCLUDED
