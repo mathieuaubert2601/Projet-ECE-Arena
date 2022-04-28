6a4af9e873b01138d201b2d0dd3189cc4d8083a0
mathieuaubert2601 <mathieu.aubert@edu.ece.fr>
2022-04-28 11:19:05 +0200

    AjoutStructure

--- header.h
+++ header.h
@@ -2,10 +2,31 @@
 #define HEADER_H_INCLUDED
 
 ///Déclaration des bibliothèques
+#include <allegro.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 ///Déclaration des structures
+
+//Structure joueur
+typedef struct joueur
+{
+    char* pseudo;
+    int ligne, colonne, numero, equipe,classe,pv,pm,pa,tourPoison,tourDodo,tourParalysie,tourPeur,tourPeur,tourBouclier,score;
+    BITMAP* bitmap
+}t_joueur;
+
+//Structure sort
+typedef struct sorts
+{
+    int degats,numero,plusMoins,chance,type,pMin,pMax,rayon,nbrPa,typePortee,typeStatus,typeSoin;
+}t_sorts;
+
+//structure case
+typedef struct cases
+{
+    int disponibleOuNon;
+}t_cases;
 
 ///Déclaration des prototypes des sous programmes
 
