757b20eaa0e6121508c6e164b9184dd76658a6b7
ThibaultL7 <thibault.leonardon@edu.ece.fr>
2022-04-30 14:23:50 +0200

    Update header

--- header.h
+++ header.h
@@ -32,7 +32,11 @@ typedef struct cases
 
 ///D�claration des prototypes des sous programmes
 void initialisationAllegro();
+<<<<<<< Updated upstream
 void menu_classes (BITMAP* classe1, BITMAP* classe2, BITMAP* classe3, BITMAP* classe4, BITMAP* classe5, BITMAP* fond, BITMAP* page,t_joueur* joueur);
+=======
+void menu_classes (BITMAP* classe1, BITMAP* classe2, BITMAP* classe3, BITMAP* classe4, BITMAP* classe5, BITMAP* fond, BITMAP* page, t_joueur* joueur);
+>>>>>>> Stashed changes
 void AffichageBouton(BITMAP* bouton,BITMAP* boutonInv, BITMAP* page,int sourcex,int sourcey,int destx,int desty,int longueur, int hauteur);
 void information_Pikachu(BITMAP* page, BITMAP* fond);
 void information_Lucario(BITMAP* page, BITMAP* fond);
