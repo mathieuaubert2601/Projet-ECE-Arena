c3569ec6d01785cc9332e8808a5710badeb9d933
ThibaultL7 <thibault.leonardon@edu.ece.fr>
2022-04-30 16:24:29 +0200

    musiquedefond

--- main.c
+++ main.c
@@ -9,8 +9,11 @@ int main()
 
     t_joueur joueur1;
 
+    int violetcity=0;
+
     ///D�claration des BITMAP
     BITMAP* fond = load_bitmap("fond1.bmp",NULL);
+    SAMPLE* musiqueFond = load_sample("musiques/VioletCity.wav");
     BITMAP* lucario = load_bitmap("lucario/lucario1.bmp",NULL);
     BITMAP* rondoudou = load_bitmap("rondoudou/rondoudou1.bmp",NULL);
     BITMAP* pikachu = load_bitmap("pikachu/pikatchu1.bmp",NULL);
@@ -20,10 +23,16 @@ int main()
     BITMAP* page=create_bitmap(1200,800);
     clear_bitmap(page);
 
+
     while(!key[KEY_ESC])
     {
         clear_bitmap(page);
-        menu_classes(pikachu,ronflex,lucario,alakazam,rondoudou,fond,page,&joueur1);
+        if (violetcity==0)
+        {
+            play_sample(musiqueFond,200,0,1000,1);
+            violetcity++;
+        }
+        menu_classes(pikachu,ronflex,lucario,alakazam,rondoudou,fond,page,&joueur1,&violetcity,musiqueFond);
         show_mouse(page);
 
         blit(page,screen,0,0,0,0,1200,800);
