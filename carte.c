939a84f16a7a27e18ac172e4b009ff6ed93e8d03
LeaEce <lea.gauthier@edu.ece.fr>
2022-05-02 09:43:55 +0200

    cases

--- carte.c
+++ carte.c
@@ -14,6 +14,11 @@ void afficher_arbre(BITMAP* buffer)
 {
     //Déclaration des variables
     BITMAP* arbre = load_bitmap("arbre1.bmp",NULL);
+    BITMAP* casefiltre = load_bitmap("casefiltre.bmp",NULL);
+
+    int casex, casey;
+    casex = -30;
+    casey = -32;
 
     //affichage des arbres sur le buffer
     masked_stretch_blit(arbre,buffer,0,0,arbre->w,arbre->h,53,195,arbre->w/5,arbre->h/5);
@@ -25,10 +30,21 @@ void afficher_arbre(BITMAP* buffer)
     masked_stretch_blit(arbre,buffer,0,0,arbre->w,arbre->h,770,225,arbre->w/5,arbre->h/5);
     masked_stretch_blit(arbre,buffer,0,0,arbre->w,arbre->h,318,0,arbre->w/5,arbre->h/5);
     masked_stretch_blit(arbre,buffer,0,0,arbre->w,arbre->h,623,385,arbre->w/5,arbre->h/5);
+
+    casebleu(casefiltre,buffer,casex*0,casey*4);
+    casebleu(casefiltre,buffer,casex*1,casey*4);
+    casebleu(casefiltre,buffer,casex*2,casey*4);
+    casebleu(casefiltre,buffer,casex*3,casey*4);
+
     show_mouse(buffer);
 }
 
 void afficher_coordonnees(BITMAP* buffer)
 {
     textprintf(buffer,font,150,150,makecol(255,255,255),"x : %d, y : %d",mouse_x/30,mouse_y/32);
+}
+
+void casebleu(BITMAP* casefiltre,BITMAP* buffer, int posx, int posy)
+{
+    blit(casefiltre,buffer,posx,posy,0,0,buffer->w,buffer->h);
 }
